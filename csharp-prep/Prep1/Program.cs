6e758b1cbc7e25b15bf9566cb8e9c87cc91246e5
Willian <will.mayer@outlook.com>
2023-09-12 23:40:24 -0300

    Finishing Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,16 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   
+        //Input the names
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name? " );
+        string lname = Console.ReadLine();
+
+        //Results
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
