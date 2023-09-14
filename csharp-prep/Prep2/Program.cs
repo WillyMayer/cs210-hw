2320d77ebbaa092f1884b190428d709aad041452
Willian <will.mayer@outlook.com>
2023-09-13 23:04:00 -0300

    Assignment 02 prepared

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        string letter = "";
+
+        if (percentage >= 90) 
+        {
+            letter = "A";
+        } else if (percentage >= 80) 
+        {
+            letter = "B";
+        } else if (percentage >= 70)
+        {
+            letter = "C";
+        } else if (percentage >= 60)
+        {
+            letter = "D";
+        } else if (percentage < 60) 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (percentage >= 70) {
+            Console.WriteLine("Congrats! You passed.");
+        } else if (percentage < 70) 
+        {
+            Console.WriteLine("Sorry, try the next time.");
+        }
     }
 }
